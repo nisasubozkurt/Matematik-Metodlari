883fbddbfbecd05289f20b94202375ea9954e605
Nisasu Bozkurt <bozkurtnisasu@hotmail.com>
2022-12-27 16:34:38 +0300

    Matematik metotları eklendi

---
+++ src/mathmetodları/MathMetodları.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mathmetodları;
+
+public class MathMetodları {
+
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-447));//mutlak değer
+        System.out.println(Math.pow(2, 3));//üs alma
+        System.out.println(Math.sqrt(25));//karekök
+        System.out.println(Math.max(10, 70));//max sayıyı döndür
+        System.out.println(Math.min(10, 70));//min sayıyı döndür
+        
+        System.out.println(Math.ceil(2.2));//bi üst büyük sayıya yuvarlar
+        System.out.println(Math.floor(2.8));//bi alt küçük sayıya yuvarlar
+        System.out.println(Math.round(2.7));//normal yuvarlama işlemi
+        System.out.println(Math.random());//random sayı üretme
+    }
+    
+}
